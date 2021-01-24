41d7e657c90a7d4a1e5ffa27d0c1be9d2e19aa34
DjianR <grojas@thelema.cl>
2021-01-24 20:05:55 -0300

    add buetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/odontomil.jpg" />
+    <PharmaList/>
+    <!-- <HelloWorld msg="Farmacos"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+// import { PharmaList } from '@/components/PharmaList.vue'
+import PharmaList from '../components/PharmaList.vue';
+// import Autocomplete from '@trevoreyre/autocomplete-vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    PharmaList
+
+  },
+};
 </script>
