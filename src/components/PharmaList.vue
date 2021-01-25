d381a6f5befd2394cfa23c5472a27b63c31b992b
DjianR <grojas@thelema.cl>
2021-01-25 05:26:28 -0300

    modificaciones db

--- src/components/PharmaList.vue
+++ src/components/PharmaList.vue
@@ -1,9 +1,9 @@
 <template>
   <button @click="traerDATA">Recargar farmacos</button>
   <ul>
-    <li v-for="(item) in farmacosArray" :key="item.id">
-      <router-link :to="{name:'DetalleFarmaco', params:{id: item.id }}">
-        {{item.title}}
+    <li v-for="(item, index) in farmacosArray" :key="item.index">
+      <router-link :to="{name:'DetalleFarmaco', params:{id: index }}">
+        {{item.mombre}}
       </router-link>
     </li>
   </ul>
@@ -24,7 +24,7 @@ export default {
     },
     async traerDATA() {
       try {
-        const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await fetch("https://raw.githubusercontent.com/DjianR/OdontoDosis/master/db.json");
         const array = await data.json();
         console.log(array);
         this.farmacosArray = array;
